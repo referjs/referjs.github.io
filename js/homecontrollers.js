277573195e56421c3257f9bb62f4fceb3778718f
inroid@discoversgroup.com <inroid@discoversgroup.com>
2015-06-11 22:27:56 +0530

    New site

--- js/homecontrollers.js
+++ js/homecontrollers.js
@@ -36,7 +36,7 @@ $scope.LoadArticles=function(){
 	
 	$http({
 		method:'POST',
-		url:'/touch/srvc/dbgetCont.php?a=a&b=b', 
+		url:'https://www.clancap.com/touch/srvc/dbgetCont.php?a=a&b=b', 
 		data:$.param({l:0}),
 		headers : {'Content-Type': 'application/x-www-form-urlencoded'}  
 
